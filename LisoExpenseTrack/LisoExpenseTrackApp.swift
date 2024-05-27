7f8fce3527822e28a344808beea756a10f304ef6
emnesty <legostr@gmail.com>
2024-05-27 17:11:03 -0300

    Initial Commit

---
+++ LisoExpenseTrack/LisoExpenseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LisoExpenseTrackApp.swift
+//  LisoExpenseTrack
+//
+//  Created by Luciano Silva on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LisoExpenseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
